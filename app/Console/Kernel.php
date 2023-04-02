52ab3efd9c90677ffe5be184547b0d960c183714
icntsy <icanatasya72@gmail.com>
2023-04-02 17:22:51 +0700

    ko

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+// use
 class Kernel extends ConsoleKernel
 {
     /**
